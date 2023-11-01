ecb8e37e67b574ec7dea31fc78488aba7a3924a5
AdryanMaikel <adryanmaikel@gmail.com>
2023-11-01 17:28:15 -0300

    first commit

---
+++ screen test.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+# sg.theme_previewer()
+sg.theme('DarkPurple2')
+layout = [
+  [sg.Text('Calcular horário de saída', text_color='white')],
+  [sg.Button('Clique em mim')]
+]
+
+window = sg.Window('Minha Janela', layout)
+
+while True:
+  event, values = window.read()
+  if event == sg.WINDOW_CLOSED:
+    break
+  if event == 'Clique em mim':
+    sg.popup('Botão Clicado')
+
+window.close()
